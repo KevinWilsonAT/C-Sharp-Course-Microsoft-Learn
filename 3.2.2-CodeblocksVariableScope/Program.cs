6a48e8e040852ff1c1355f87bdf4ba7830b5bc11
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2025-01-02 16:06:53 -0300

    Chore: Update class 02-01-25

---
+++ 3.2.2-CodeblocksVariableScope/Program.cs
@@ -0,0 +1,33 @@
+﻿bool flag = true;
+int value = 0;
+
+if (flag)
+{
+    Console.WriteLine($"Inside the code block: {value}");
+}
+
+value = 10;
+Console.WriteLine($"Outside the code block: {value}");
+
+// Code sample 1
+bool flag = true;
+int value;
+
+if (flag)
+{
+    value = 10;
+    Console.WriteLine($"Inside the code block: {value}");
+}
+
+Console.WriteLine($"Outside the code block: {value}");
+
+// Code sample 2
+int value;
+
+if (true)
+{
+    value = 10;
+    Console.WriteLine($"Inside the code block: {value}");
+}
+
+Console.WriteLine($"Outside the code block: {value}");
